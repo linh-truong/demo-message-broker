44f0e200e2a3e84de050c5524fa4debc001a50d8
Linh Truong <linhtv.uit@gmail.com>
2018-09-15 14:50:43 +0700

    Simple rabbitmq implementation

--- node1/index.js
+++ node1/index.js
@@ -1,3 +1,4 @@
+const amqp = require('amqplib/callback_api');
 const express = require('express');
 const app = express();
 
@@ -5,9 +6,18 @@ const messages = []
 
 app.get('/', (req, res) => {
     return res.json({
-        name:'Node 1',
         data: messages
     })
 });
 
-app.listen(3001, () => console.log('Node 1 listening on port 3001!'));
+app.listen(3000, () => console.log('Node listening on port 3000!'));
+
+amqp.connect('amqp://rabbitmq:rabbitmq@demo-rabbitmq:5672', function (err, conn) {
+    conn.createChannel(function (err, channel) {
+        const queueName = 'demo-queue';
+        channel.assertQueue(queueName, { durable: false });
+        setInterval(()=>{
+            channel.sendToQueue(queueName, new Buffer(`Message ${new Date()}`));
+        }, 10000)
+    });
+});
--- node2/index.js
+++ node2/index.js
@@ -1,3 +1,4 @@
+const amqp = require('amqplib/callback_api');
 const express = require('express');
 const app = express();
 
@@ -5,9 +6,18 @@ const messages = []
 
 app.get('/', (req, res) => {
     return res.json({
-        name:'Node 2',
         data: messages
     })
 });
 
-app.listen(3002, () => console.log('Node 2 listening on port 3002!'));
+app.listen(3000, () => console.log('Node listening on port 3000!'));
+
+amqp.connect(process.env.RABBITMQ_URI, function (err, conn) {
+    conn.createChannel(function (err, channel) {
+        const queueName = 'demo-queue';
+        channel.assertQueue(queueName, { durable: false });
+        channel.consume(queueName, message => {
+            messages.push(message);
+        })
+    });
+});
