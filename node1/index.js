207661585d5e5c28a7b66b4ee467bc29b9d0d93e
Linh Truong <linhtv.uit@gmail.com>
2018-09-16 15:26:10 +0700

    Add message sender, receiver. Setup rabbitMQ, kafka with docker compose

--- node1/index.js
+++ node1/index.js
@@ -1,13 +1,43 @@
-const express = require('express');
+const amqp = require("amqplib/callback_api");
+var Kafka = require("no-kafka");
+const express = require("express");
+
 const app = express();
-
-const messages = []
-
-app.get('/', (req, res) => {
-    return res.json({
-        name:'Node 1',
-        data: messages
-    })
+app.get("/", (req, res) => {
+  return res.json({
+    name: "Node 1"
+  });
 });
+app.listen(3001, () => console.log("Node 1 listening on port 3001!"));
 
-app.listen(3001, () => console.log('Node 1 listening on port 3001!'));
+// RabbitMQ
+amqp.connect(
+  "amqp://rabbitmq:rabbitmq@localhost:5672",
+  (err, conn) => {
+    conn.createChannel(function(err, channel) {
+      const queueName = "demo-queue";
+      channel.assertQueue(queueName, { durable: false });
+      setInterval(() => {
+        channel.sendToQueue(queueName, new Buffer(`RabbitMQ ${Date.now()}`));
+        console.log('Sent RabbitMQ message')
+      }, 10000);
+    });
+  }
+);
+
+Kafka
+var producer = new Kafka.Producer({
+  connectionString: "localhost:9092"
+});
+producer.init().then(() => {
+  setInterval(() => {
+    producer.send({
+      topic: "demo-topic",
+      partition: 0,
+      message: {
+        value: `Kafka ${Date.now()}`
+      }
+    });
+    console.log('Sent Kafka message')
+  }, 10000);
+});
--- node2/index.js
+++ node2/index.js
@@ -1,13 +1,48 @@
-const express = require('express');
+const amqp = require("amqplib/callback_api");
+var Kafka = require("no-kafka");
+const express = require("express");
+
+let rabbitMQMessages = [];
+let kafkaMessages = [];
+
 const app = express();
-
-const messages = []
-
-app.get('/', (req, res) => {
-    return res.json({
-        name:'Node 2',
-        data: messages
-    })
+app.get("/rabbitmq", (req, res) => {
+  return res.json({
+    name: "Node 2",
+    data: rabbitMQMessages
+  });
 });
+app.get("/kafka", (req, res) => {
+  return res.json({
+    name: "Node 2",
+    data: kafkaMessages
+  });
+});
+app.listen(3002, () => console.log("Node 2 listening on port 3002!"));
 
-app.listen(3002, () => console.log('Node 2 listening on port 3002!'));
+// RabbitMQ
+amqp.connect(
+  "amqp://rabbitmq:rabbitmq@localhost:5672",
+  function (err, conn) {
+    conn.createChannel(function (err, channel) {
+      const queueName = "demo-queue";
+      channel.assertQueue(queueName, { durable: false });
+      channel.consume(queueName, message => {
+        rabbitMQMessages.push(message);
+        console.log('Received RabbitMQ message');
+      });
+    });
+  }
+);
+
+// Kafka
+const consumer = new Kafka.SimpleConsumer({
+  connectionString: "localhost:9092"
+});
+consumer.init().then(function () {
+  // Subscribe partitions 0 in a topic:
+  consumer.subscribe("demo-topic", [0], (messageSet, topic, partition) => {
+    kafkaMessages.push.apply(kafkaMessages, messageSet);
+    console.log('Received Kafka message');
+  });
+});
